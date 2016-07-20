220794e21790bd8784f5a50f8bea3f64afbbfc37
poc <poc7667@gmail.com>
2016-07-20 15:41:32 -0700

    toogle class

--- Kanban/components/Card.jsx
+++ Kanban/components/Card.jsx
@@ -2,16 +2,47 @@ import React from 'react';
 import CheckList from './CheckList.jsx';
 
 class Card extends React.Component{
+
+    constructor(props, context){
+        super(props, context);
+        this.state = {
+            showDetails: false,
+            class_for_card_title: "card_title"
+        };
+    }
+
+    toggleDetails(){
+        this.setState({
+            showDetails: !this.state.showDetails
+        })
+        if(this.state.showDetails===true){
+            this.setState({class_for_card_title: "card_title_is_open"})
+        }else{
+            this.setState({class_for_card_title: "card_title"})
+        }
+    }
+
     render(){
         return (
-            <div className="card">
-                <div className="card_title">{this.props.title}</div>
-                <div className="card_details">
-                    {this.props.description}
-                    <CheckList cardId={this.props.id} 
-                    tasks={this.props.tasks}>
-                    </CheckList>
+
+            <div className="card" onClick={this.toggleDetails.bind(this)}>
+                <h1> {this.state.showDetails? "SHOW" : "HIDE"}</h1>
+                <div  className={this.state.class_for_card_title}>
+
+                    {this.props.title}
                 </div>
+                {
+                    this.state.showDetails?
+                        <div className="card_details">
+                            {this.props.description}
+                            <CheckList cardId={this.props.id} 
+                            tasks={this.props.tasks}>
+                            </CheckList>
+                        </div>                    
+                    :
+                        <div/>
+                }
+
             </div>
             );
     }
